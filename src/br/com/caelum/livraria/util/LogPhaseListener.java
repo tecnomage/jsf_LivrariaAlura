0d23001a79e8599d80b46a86640ea6e6f88a730b
tecnomage <tecnocratay@gmail.com>
2015-11-05 18:03:28 -0200

    adicionando o phaselistener e implementando o uso de faceless ao projeto

--- src/br/com/caelum/livraria/util/LogPhaseListener.java
+++ src/br/com/caelum/livraria/util/LogPhaseListener.java
@@ -4,22 +4,21 @@ import javax.faces.event.PhaseEvent;
 import javax.faces.event.PhaseId;
 import javax.faces.event.PhaseListener;
 
+import sun.launcher.resources.launcher;
+
 public class LogPhaseListener  implements PhaseListener {
 
-	
-	
-	
-	
+	private static final long serialVersionUID = 1L;
 	
 	@Override
 	public void afterPhase(PhaseEvent event) {
-		System.out.println("FASE1" + event.getPhaseId());
+		//System.out.println("FASE1" + event.getPhaseId());
 		
 	}
 
 	@Override
 	public void beforePhase(PhaseEvent event) {
-		System.out.println("FASE" + event.getPhaseId());
+		System.out.println("FASE " + event.getPhaseId());
 		
 	}
 
